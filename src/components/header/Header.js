577c14b097d30ba15aa46caa154a399df73919f4
Khanna <52990742+khannapro@users.noreply.github.com>
2023-11-28 16:12:57 +0530

    update protfolio new files

---
+++ src/components/header/Header.js
@@ -0,0 +1,49 @@
+import React, {useContext} from "react";
+import Headroom from "react-headroom";
+import "./Header.scss";
+import StyleContext from "../../contexts/StyleContext";
+import {
+  greeting,
+  workExperiences,
+  skillsSection,
+  openSource,
+  blogSection,
+  talkSection,
+  achievementSection
+} from "../../portfolio";
+
+function Header() {
+  const {isDark} = useContext(StyleContext);
+  const viewExperience = workExperiences.display;
+  const viewOpenSource = openSource.display;
+  const viewSkills = skillsSection.display;
+  const viewAchievement = achievementSection.display;
+  const viewBlog = blogSection.display;
+  const viewTalks = talkSection.display;
+
+  return (
+    <Headroom>
+      <header className={isDark ? "dark-menu header" : "header"}>
+        <a href="/" className="logo">
+          <span className="logo-name">{greeting.username}</span>
+        </a>
+        <ul className={isDark ? "dark-menu menu" : "menu"}>
+          {viewSkills && (
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+          )}
+          {viewExperience && (
+            <li>
+              <a href="#experience">Work Experiences</a>
+            </li>
+          )}
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+        </ul>
+      </header>
+    </Headroom>
+  );
+}
+export default Header;
